37dfc9795924fa94a85772f269c0e7d1700f1fed
Jorge Mira <jorge@gbyteinfo.com.br>
2023-11-16 01:38:03 -0300

    Enviando aula 6

--- Aula6.py
+++ Aula6.py
@@ -1,15 +1,43 @@
+#Imports
 import pandas as pd
-#Padrao para desenvolvimento
-print("\n********Análise Exploratória Básica********")
-print("*******************************************\n")
 
-# Carregar os dados do arquivo CSV corrigido do GitHub
 
-print("\n********Análise Exploratória Básica********")
+# Carregar os dados do arquivo CSV do GitHub
+print("\n********Manipulando CSV para memoria o Volume de Dados********")
 dados = "https://raw.githubusercontent.com/Professor-Leonardo/DADOS/main/C%C3%B3pia%20de%20Discrep%C3%A2ncias_MAPGEO2015%20(Dados%20tabelados).csv"
-df_ibge = pd.read_csv(dados, sep=';', encoding='ISO-8859-1')
-df_ibge.head()
+df_csv = pd.read_csv(dados, sep=';', encoding='ISO-8859-1')
 print("*******************************************\n")
-# Salvando o gráfico na pasta especificada
+
+print("\n********Análise Exploratória Básica********")
+print(df_csv.head())
+print("*******************************************\n")
+
+print("\n*****informações básicas sobre o DataFrame:*****")
+print(df_csv.info())
+print("*******************************************\n")
+
+print("\n*****Algumas Estatísticas Descritivas:*****")
+print(df_csv.describe())
+print("*******************************************\n")
+
+
+# Carregar os dados do arquivo XLSX do GitHub
+print("\n********Manipulando XLSX para memoria o Volume de Dados********")
+url_excel = "https://raw.githubusercontent.com/Professor-Leonardo/DADOS/main/C%C3%B3pia%20de%20Discrep%C3%A2ncias_MAPGEO2015%20(Dados%20tabelados).xlsx"
+df_excel = pd.read_excel(url_excel)
+print("*******************************************\n")
+
+print("\n********Análise Exploratória Básica********")
+print(df_excel.head())
+print("*******************************************\n")
+
+print("\n*****informações básicas sobre o DataFrame:*****")
+print(df_excel.info())
+print("*******************************************\n")
+
+print("\n*****Algumas Estatísticas Descritivas:*****")
+print(df_excel.describe())
+print("*******************************************\n")
+
 
 
