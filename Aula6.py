517c92361e3a25f3e46d875baeca2080956a75e4
Jorge Mira <jorge@gbyteinfo.com.br>
2023-11-16 02:00:19 -0300

    Ajustes Aula 6

--- Aula6.py
+++ Aula6.py
@@ -4,12 +4,12 @@ import pandas as pd
 
 # Carregar os dados do arquivo CSV do GitHub
 print("\n********Manipulando CSV para memoria o Volume de Dados********")
-dados = "https://raw.githubusercontent.com/Professor-Leonardo/DADOS/main/C%C3%B3pia%20de%20Discrep%C3%A2ncias_MAPGEO2015%20(Dados%20tabelados).csv"
+dados = "https://raw.githubusercontent.com/gbyteinfo/Aulas-Big-Data-Unimetrocamp/develop/DataCidadeEstadoTemperatura.csv"
 df_csv = pd.read_csv(dados, sep=';', encoding='ISO-8859-1')
 print("*******************************************\n")
 
 print("\n********Análise Exploratória Básica********")
-print(df_csv.head())
+print(df_csv.head(10))
 print("*******************************************\n")
 
 print("\n*****informações básicas sobre o DataFrame:*****")
@@ -23,12 +23,12 @@ print("*******************************************\n")
 
 # Carregar os dados do arquivo XLSX do GitHub
 print("\n********Manipulando XLSX para memoria o Volume de Dados********")
-url_excel = "https://raw.githubusercontent.com/Professor-Leonardo/DADOS/main/C%C3%B3pia%20de%20Discrep%C3%A2ncias_MAPGEO2015%20(Dados%20tabelados).xlsx"
+url_excel = "https://github.com/gbyteinfo/Aulas-Big-Data-Unimetrocamp/raw/develop/DataCidadeEstadoTemperatura.xlsx"
 df_excel = pd.read_excel(url_excel)
 print("*******************************************\n")
 
 print("\n********Análise Exploratória Básica********")
-print(df_excel.head())
+print(df_excel.head(10))
 print("*******************************************\n")
 
 print("\n*****informações básicas sobre o DataFrame:*****")
